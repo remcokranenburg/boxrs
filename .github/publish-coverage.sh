b804b64c16efe9d367d4b0cdf68b38e161ff33e6
Remco Kranenburg <remco@burgsoft.nl>
2023-03-10 15:12:47 +0100

    Add main branch tests with additional privileges

--- .github/publish-coverage.sh
+++ .github/publish-coverage.sh
@@ -2,9 +2,11 @@
 
 set -eu
 
-cp -r target/debug/coverage/ /tmp/coverage/
-git checkout artifacts
-rm -r coverage/
-cp -r /tmp/coverage/ coverage/
-git add . && git commit -m "Update coverage report for $GITHUB_SHA"
+git config user.name github-actions
+git config user.email github-actions@github.com
+
+rm -r coverage/ || true
+cp -r ../main/target/debug/coverage/ coverage/
+git add .
+git commit -m "Update coverage report for $GITHUB_SHA"
 git push
