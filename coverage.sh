4ad11b2f2e8b84f3e3753cd08fb061daf4cb0b01
Remco Kranenburg <remco@burgsoft.nl>
2023-04-05 23:31:38 +0200

    coverage: ignore build folder

--- coverage.sh
+++ coverage.sh
@@ -14,4 +14,4 @@ popd
 LLVM_PROFILE_FILE="target/coverage/boxrs-%p-%m.profraw" cargo test --verbose
 
 rm -r target/debug/coverage/
-target/grcov . -s . --binary-path target/debug/ -t html --branch --ignore-not-existing -o target/debug/coverage/
+target/grcov . -s . --binary-path target/debug/ -t html --branch --ignore-not-existing --ignore 'target/*' -o target/debug/coverage/
